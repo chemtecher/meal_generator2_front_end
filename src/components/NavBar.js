9d225c14d45f4529576e17bb6e2bd1f6b5232982
ashanti <ammanier2@gmail.com>
2023-02-05 16:03:39 -0500

    added UserForm pop up logic

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,15 +1,20 @@
-import React from 'react'
+import { useState } from 'react'
+import UserForm from './UserForm'
 
 function NavBar() {
+  const [isOpen, setIsOpen] = useState(false)
 
+  const togglePopUpForm = () => {
+    setIsOpen(!isOpen)
+  }
   return (
     <nav>
       <img src="./images/chefHat.png" alt="chef hat"/>
       <h1 className="nav-title">Bon AppeType!</h1>
       <div className="nav-options" >
-        <h3>
+        <h3 onClick={togglePopUpForm}>
           Sign In
-          onClick={}
+          {isOpen && <UserForm />}
         </h3>
         <h3 className="nav-about_us">About Us</h3>
         <h3>Log Out</h3>
