dc30254d6139cea39e7a1219eab2c316d912da93
ashanti <ammanier2@gmail.com>
2023-02-05 15:17:16 -0500

    added UserForm component and logic

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,16 @@
 import React from 'react'
-import chefHat from 
+
 function NavBar() {
+
   return (
     <nav>
-      <img src="./images/servings_icon.png" alt="chef hat"/>
+      <img src="./images/chefHat.png" alt="chef hat"/>
       <h1 className="nav-title">Bon AppeType!</h1>
       <div className="nav-options" >
-        <h3>Sign In</h3>
+        <h3>
+          Sign In
+          onClick={}
+        </h3>
         <h3 className="nav-about_us">About Us</h3>
         <h3>Log Out</h3>
         <h3>Saved Recipes</h3>
--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -1,8 +1,70 @@
-import React from 'react'
+import {useState} from 'react'
 
 function UserForm() {
+  const [formData, setFormData] = useState(
+    {
+      email: "",
+      password: "",
+      confirmPassword: ""
+  })
+
+  const handleChange = (event) => {
+    const {name, value} = event.target
+    setFormData(prevFormData => {
+      return{
+        ...prevFormData,
+        [name] : value
+      }
+    })
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    if (formData.confirmPassword !== formData.password) {
+      console.log("Passwords do not match")
+      // return to exit from function
+      // return true or false to  be used in another function for API call??
+      return false
+    }
+    else {
+      console.log("Sign up sccessful")
+      return true
+    }
+  } 
+
   return (
-    <div>UserForm</div>
+    <div className="form-container" onSubmit={handleSubmit}>UserForm
+      <form>
+        <input
+          type="email"
+          placeholder="Email address"
+          className="form-input email"
+          name="email"
+          onChange={handleChange}
+          value={formData.email}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          className="form-input password"
+          name="password"
+          onChange={handleChange}
+          value={formData.email}
+        />
+        <input
+          type="password"
+          placeholder="Confirm Password"
+          className="form-input confirmation"
+          name="confirmPassword"
+          onChange={handleChange}
+          value={formData.confirmPassword}
+        />
+        <button className="button form-submit">
+          Sign Up
+        </button>
+      </form>
+
+    </div>
   )
 }
 
