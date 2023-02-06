82287f6713ee124b8c24304c45239a79463b71e4
ashanti <ammanier2@gmail.com>
2023-02-05 21:50:26 -0500

    removed 'closed button from popup form

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,20 +7,21 @@ function NavBar() {
   // create state 
   const [signInClicked, setSignInClicked] = useState(false)
 
-  // Event handler function that runs when Sign In is clicked
-  // toggles the value of isOpen state 
-
+  // Event handler function that runs when "Sign In" in nav bar is clicked
+  // toggles the value of signInClicked state 
   const togglePopUpForm = () => {
+    // setSignInClicked(true)
     setSignInClicked(!signInClicked)
   }
+  
+
   return (
     <nav>
-      <img src='./' alt="chef hat"/>
+      <img src="" alt="chef hat"/>
       <h1 className="nav-title">Bon AppeType!</h1>
       <div className="nav-options">
         <h3 onClick={togglePopUpForm}>
           Sign In
-          {/* {signInClicked && <UserForm />} */}
           {signInClicked && <UserForm />}
         </h3>
         <h3 className="nav-about_us">About Us</h3>
