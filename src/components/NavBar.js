966aa9d0ed696cff871987f71b1f29247cf268ec
Ryan Thomas <rrthomas1913@gmail.com>
2023-02-07 19:32:50 -0500

    Added functionality of the SavedRecipes component & made succesful API calls

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,19 +1,32 @@
 import { useState } from 'react'
 import PopUp from './PopUp'
 import UserForm from './UserForm'
+import SavedRecipes from './SavedRecipes'
+import axios from 'axios'
 
 
 function NavBar() {
   // create state 
   const [signInClicked, setSignInClicked] = useState(false)
-
+  const [savedRecipesData, setSavedRecipeData] = useState([])
+  let user_id = 3;
+  
   // Event handler function that runs when "Sign In" in nav bar is clicked
   // toggles the value of signInClicked state 
   const togglePopUpForm = () => {
     // setSignInClicked(true)
     setSignInClicked(!signInClicked)
   }
-  
+  const getSavedRecipes = (user_id) => {
+      axios.get(`${process.env.REACT_APP_BACKEND_URL}/user/${user_id}/recipes`)
+      .then((response) => {
+        setSavedRecipeData(response.data)
+        console.log(response.data, "All Saved Recipes")
+      })
+      .catch((error) => {
+        console.log(error)
+    })
+    }
 
   return (
     <nav>
@@ -26,7 +39,14 @@ function NavBar() {
         </h3>
         <h3 className="nav-about_us">About Us</h3>
         <h3>Log Out</h3>
-        <h3>Saved Recipes</h3>
+        {/* <h3>Saved Recipes</h3>  */}
+        <SavedRecipes 
+          savedRecipesData={savedRecipesData} 
+          setSavedRecipeData={setSavedRecipeData}
+          getSavedRecipes={getSavedRecipes}
+          user_id={user_id}
+        /> 
+        {/* need onclick for saved button to send you to saved recipes */}
       </div>
     </nav>
   )
