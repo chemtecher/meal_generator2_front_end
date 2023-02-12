f8e2b42f641dd811f8d4120a96524af662b5983c
ashanti <ammanier2@gmail.com>
2023-02-12 13:17:41 -0500

    still working on updating favsList after deleting recipe

--- src/components/RemoveButton.js
+++ src/components/RemoveButton.js
@@ -2,17 +2,14 @@ import React from 'react'
 import axios from 'axios';
 
 function RemoveButton({favRecipe, getSavedRecipes}) {
-  const removeFromFav = () => {
-    try {
-      axios.delete(`${process.env.REACT_APP_BACKEND_URL}/search_recipes/remove/${favRecipe.recipe_id}`);
+  const removeFromFav = async () => {
+      await axios.delete(`${process.env.REACT_APP_BACKEND_URL}/search_recipes/remove/${favRecipe.recipe_id}`);
       console.log("Recipe removed from saved")
-      getSavedRecipes()
-      
+      .then(() => {
+        getSavedRecipes()
+      })
     } 
-    catch (error) {
-      console.log(error)
-    }
-  }
+  
   // const removeFromFav = () => {
   //   try {
   //     axios.delete(`${process.env.REACT_APP_BACKEND_URL}/search_recipes/remove/${favRecipe.recipe_id}`);
