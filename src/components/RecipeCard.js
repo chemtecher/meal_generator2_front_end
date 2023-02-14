943e49585597440c1b46241b23689b76a05773e3
Anjula Tamang <parkhrinj@gmail.com>
2023-02-14 14:29:45 -0500

    url links are added to title

--- src/components/RecipeCard.js
+++ src/components/RecipeCard.js
@@ -8,22 +8,24 @@ function RecipeCard(props) {
   
   return (
     <div>
-    
-      <img className="recipe-img" src={image} alt="food dish"/>
-      <h2>{title}</h2>
-      <img 
-        className="recipe-servings-img" 
-        src={require('../images/servings_icon.png')} 
+      <img className="recipe-img" src={image} alt="food dish" />
+      <h2>
+        <a className= "recipe-url" href={recipe_url}>{title}</a>
+      </h2>
+      <img
+        className="recipe-servings-img"
+        src={require("../images/servings_icon.png")}
         alt="plate and fork"
-        /> {servings} servings
+      />{" "}
+      {servings} servings
       <img
         className="recipe-time-img"
         src={require("../images/time_icon.png")}
         alt="clock"
-      /> {time} mins
+      />{" "}
+      {time} mins
     </div>
-
-  )
+  );
 }
 
 export default RecipeCard
